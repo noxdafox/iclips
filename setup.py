e48e84cd1ab07e40512dd8bccd81f1d9f18f3ab7
noxdafox <noxdafox@gmail.com>
2017-12-23 13:04:01 +0200

    setup: fixes and improvements
    Add sys.prefix to kernel data file.
    
    Signed-off-by: noxdafox <noxdafox@gmail.com>

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 import os
+import sys
 
 from setuptools import setup, find_packages
 
 
 setup(
-    name="ICLIPS",
-    version="0.0.1",
-    author="Matteo Cafasso",
-    author_email="noxdafox@gmail.com",
-    description=("CLIPS Jupyter console"),
-    license="GPL",
-    keywords="clips expert-system jupyter",
+    name='ICLIPS',
+    version='0.0.1',
+    author='Matteo Cafasso',
+    author_email='noxdafox@gmail.com',
+    description=('CLIPS Jupyter console'),
+    license='GPL',
+    keywords='clips expert-system jupyter',
     install_requires=[
-        "clipspy",
-        "jupyter-console"
+        'clipspy',
+        'jupyter-console'
     ],
     data_files=[
-        (os.path.join('share', 'jupyter', 'kernels', 'clips'), 'kernel.json')
+        (os.path.join(sys.prefix, 'share/jupyter/kernels/clips'),
+         ['kernel.json'])
     ],
-    packages=find_packages(exclude=["test"]),
+    packages=find_packages(),
     entry_points={
         'pygments.lexers': [
             'clips=iclips.clips_pygments:CLIPSLexer'
