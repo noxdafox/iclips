7268e1a15d6502f3c92fbd9a4e6fb67349f011e8
noxdafox <noxdafox@gmail.com>
2017-12-28 01:38:40 +0200

    Update setup.py
    Add tag based versioning.
    
    Add README based long description.
    
    Add url and classifiers.
    
    Signed-off-by: noxdafox <noxdafox@gmail.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,48 @@
 import os
 import sys
+import subprocess
 
 from setuptools import setup, find_packages
 
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def package_version():
+    """Get the package version via Git Tag."""
+    version_path = os.path.join(os.path.dirname(__file__), 'version.py')
+
+    version = read_version(version_path)
+    write_version(version_path, version)
+
+    return version
+
+
+def read_version(path):
+    try:
+        return subprocess.check_output(('git', 'describe')).rstrip().decode()
+    except subprocess.CalledProcessError:
+        with open(path) as version_file:
+            version_string = version_file.read().split('=')[-1]
+            return version_string.strip().replace('"', '')
+
+
+def write_version(path, version):
+    msg = '"""Versioning controlled via Git Tag, check setup.py"""'
+    with open(path, 'w') as version_file:
+        version_file.write(msg + os.linesep + os.linesep +
+                           '__version__ = "{}"'.format(version) +
+                           os.linesep)
+
+
 setup(
     name='iCLIPS',
-    version='0.0.1',
+    version="{}".format(package_version()),
     author='Matteo Cafasso',
     author_email='noxdafox@gmail.com',
     description=('CLIPS Jupyter console'),
+    long_description=read('README.rst'),
     license='GPL',
     keywords='clips expert-system jupyter',
     install_requires=[
@@ -25,5 +58,13 @@ setup(
         'pygments.lexers': [
             'clips=iclips.clips_pygments:CLIPSLexer'
         ]
-    }
+    },
+    url="https://github.com/noxdafox/iclips",
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: " +
+        "GNU General Public License v3 or later (GPLv3+)"
+    ]
 )
