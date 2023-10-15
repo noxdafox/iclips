8db935d8e1ccc44c8e45e8d30db256ab1a1a2525
Matteo Cafasso <noxdafox@gmail.com>
2023-10-15 18:45:29 +0300

    doc: update copyright year
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'iCLIPS'
-copyright = '2017-2021, Matteo Cafasso'
+copyright = '2017-2023, Matteo Cafasso'
 author = 'Matteo Cafasso'
 
 
